6b1877a695fd0a91487e4438dbb584fc77ee3776
artoftadashi <samuelolanrewaju05@gmail.com>
2024-03-23 14:55:53 +0000

    bug fixes

--- index.js
+++ index.js
@@ -2,29 +2,34 @@ const woven = document.querySelector('.woven');
 
 woven.addEventListener('mouseover', () => {
     woven.setAttribute('src', './images/scary.png');
-    document.querySelector('.hover-btn').style.display = "block";
+    $('.btn3').addClass('active');
 });
 
 woven.addEventListener('mouseleave', () => {
     woven.setAttribute('src', './images/gallery4.png');
+    $('.btn3').removeClass('active');
 });
 
 const reimagine = document.querySelector('.reimagine');
 
 reimagine.addEventListener('mouseover', () => {
     reimagine.setAttribute('src', './images/smile.png');
+    $('.btn2').addClass('active');
 });
 
 reimagine.addEventListener('mouseleave', () => {
     reimagine.setAttribute('src', './images/gallery3.png');
+    $('.btn2').removeClass('active');
 });
 
 const bade = document.querySelector('.bade');
 
 bade.addEventListener('mouseover', () => {
     bade.setAttribute('src', './images/bade.png');
+    $('.btn1').addClass('active');
 });
 
 bade.addEventListener('mouseleave', () => {
     bade.setAttribute('src', './images/featured.png');
+    $('.btn1').removeClass('active');
 });
